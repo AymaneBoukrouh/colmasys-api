f8eee553519583fe5d036dc17dfc3f9825a7ba10
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-06-18 22:24:16 +0100

    fix bug

--- colmasys/endpoints/crud/student.py
+++ colmasys/endpoints/crud/student.py
@@ -2,6 +2,7 @@ from colmasys import app, get_async_session
 from colmasys.models import Account, AccountModel, StudentModel, Student, Professor
 from colmasys.core import auth_required
 from colmasys.utils.account import get_account_by, register_student, get_students
+from colmasys.utils.student import get_student_by
 from fastapi import Request, Depends, HTTPException
 from sqlalchemy import select
 
@@ -28,7 +29,8 @@ async def get_student_by_id(account_id: int, async_session=Depends(get_async_ses
 async def get_user_by_username(username: str, async_session=Depends(get_async_session), _=Depends(auth_required.admin_auth_required)):
     async with async_session() as session, session.begin():
         if (account := await get_account_by(session, username=username)):
-            return account.serialize()
+            student = await get_student_by(session, id=account.user.id)
+            return student.serialize()
         else:
             raise HTTPException(status_code=404, detail='Student Not Found')
 
